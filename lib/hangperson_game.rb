045efa9f07ccfb8cc9301c7fd54c15218b0963f4
Johann Norsa <johann.norsa.pub@gmail.com>
2015-11-02 13:37:32 +0000

    Pendu

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,8 +8,50 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  attr_accessor :word_with_guesses
+  
+  
   def initialize(word)
+    @attempt = 0
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
+    @word_with_guesses = ""
+    @word.chars.each do |l| 
+      @word_with_guesses += "-"
+    end
+  end
+  
+  def guess(letter)
+    if !(letter =~ /^[a-zA-Z]+$/) then
+      raise ArgumentError
+    end
+    low_letter = letter.downcase
+    if (@word =~ Regexp.new(low_letter)) then
+      if !(@guesses =~ Regexp.new(low_letter)) then 
+        @guesses += low_letter 
+        for i in 0..@word.length-1
+          if (@word[i]==low_letter) then @word_with_guesses[i]=low_letter end
+        end
+        return true
+      end
+    else
+      if !(@wrong_guesses =~ Regexp.new(low_letter)) then 
+        @wrong_guesses += low_letter 
+        @attempt += 1
+        return true
+      end
+    end
+    return false
+  end
+  
+  def check_win_or_lose()
+    if @attempt >= 7 then return :lose end
+    if (@word == @word_with_guesses) then return :win end 
+    return :play 
   end
 
   def self.get_random_word
